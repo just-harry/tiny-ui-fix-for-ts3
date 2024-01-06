418f4a2557194b65d56d1183098749f811b2e1bb
Harry Gillanders <contact@harrygillanders.com>
2024-01-06 02:03:54 +0000

    Update the version-number to 1.3.1

--- TinyUIFixForTS3/Patch/AssemblyInfo.cs
+++ TinyUIFixForTS3/Patch/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using Sims3.SimIFace;
 [assembly: AssemblyTitle("TinyUIFixForTS3")]
 [assembly: AssemblyDescription("")]
 [assembly: Tunable]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.1.0")]
 [assembly: Guid("9c7a6368-dab2-4e40-977e-715b43a1df1b")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
