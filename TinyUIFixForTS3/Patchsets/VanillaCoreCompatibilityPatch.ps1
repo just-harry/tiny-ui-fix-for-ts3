cbc3d96b67eb0cbe018b53541b168f43f7c84394
Harry Gillanders <contact@harrygillanders.com>
2024-09-14 02:37:56 +0100

    Update the version-number of the `VanillaCoreCompatibilityPatch` patchset to 1.1.0.

--- TinyUIFixForTS3/Patchsets/VanillaCoreCompatibilityPatch.ps1
+++ TinyUIFixForTS3/Patchsets/VanillaCoreCompatibilityPatch.ps1
@@ -8,7 +8,7 @@
 #>
 
 $ID = 'VanillaCoreCompatibilityPatch'
-$Version = '1.0.3'
+$Version = '1.1.0'
 $PatchsetDefinitionSchemaVersion = 1
 
 ${If it's Christmas!} = $($Now = [DateTime]::Now; if ($Now.Month -eq 12 -and $Now.Day -eq 25) {'<hr>Merry Christmas! :)'} else {''})
